7c11bbacf6f39d88c08daa64e4a7bcf097930f84
Antoine LHOTE <alhote@e2r2p13.42.fr>
2016-03-02 17:48:52 +0100

    update

--- draw/draw_segment.c
+++ draw/draw_segment.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 14:21:15 by alhote            #+#    #+#             */
-/*   Updated: 2016/02/25 11:14:36 by alhote           ###   ########.fr       */
+/*   Updated: 2016/03/02 17:47:10 by alhote           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static int		c2(t_segment *seg, t_world *w)
 			cumul -= fabs(seg->p[1]->y2d - seg->p[0]->y2d);
 			pos[0] += inc[0];
 		}
-		if (!safe_pixel_put(w, pos[0], pos[1], seg->p[0]->color + seg->p[1]->color))
+		if (!safe_pixel_put(w, pos[0], pos[1], seg->p[0]->color * w->h / 1 + seg->p[1]->color * w->h / 1))
 			return (0);
 		++i;
 	}
@@ -63,7 +63,7 @@ static int		c1(t_segment *seg, t_world *w)
 			cumul -= fabs(seg->p[1]->x2d - seg->p[0]->x2d);
 			pos[1] += inc[1];
 		}
-		if (!safe_pixel_put(w, pos[0], pos[1], seg->p[0]->color + seg->p[1]->color))
+		if (!safe_pixel_put(w, pos[0], pos[1], seg->p[0]->color * w->h * 10 / 1 + seg->p[1]->color * w->h * 10 / 1))
 			return (0);
 		++i;
 	}
