c409be760d259c1c22a2279070bfd76997e0cfd8
Antoine LHOTE <alhote@e2r3p20.42.fr>
2016-03-05 22:24:52 +0100

    update

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 16:34:21 by alhote            #+#    #+#             */
-/*   Updated: 2016/03/05 21:51:42 by alhote           ###   ########.fr       */
+/*   Updated: 2016/03/05 22:23:49 by alhote           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,16 +62,16 @@ t_map						*init_map(char *path, t_world *w)
 	{
 		if (i < 0)
 			return (0);
-		i = -1;
 		temp = ft_strtrim(temp);
+		i = (ft_wordnbr(temp, ' ') && temp[0] != ' ' ? -1 : 0);
 		while (++i <= ft_wordnbr(temp, ' '))
 		{
 			add_point(w, i, ft_atoi(ft_strsplit(temp, ' ')[i]), m->sizey);
 			w->p->color += ft_atoi(ft_strsplit(temp, ' ')[i]) * 5;
 			seg(w, i, m->sizex, m->sizey);
 		}
-		m->sizey++;
-		m->sizex = i;
+		m->sizey += (ft_wordnbr(temp, ' ') && temp[0] != ' ' ? 1 : 0);
+		m->sizex = (ft_wordnbr(temp, ' ') && temp[0] != ' ' ? i : m->sizex);
 	}
 	return (m->sizey - 1 ? m : 0);
 }
